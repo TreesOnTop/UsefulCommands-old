f1d800f7f0a9e34a2bf3680cfcfd5c107cf3e092
Anthropoworphous <kevin1050914@gmail.com>
2021-12-07 09:51:23 -0500

    edited some import, god know how you messed those up

--- src/main/java/com/github/zac694/usefulcommands/events/EntityDamage.java
+++ src/main/java/com/github/zac694/usefulcommands/events/EntityDamage.java
@@ -1,14 +1,13 @@
 package com.github.zac694.usefulcommands.events;
 
 import com.github.zac694.usefulcommands.ConfigHandler;
-import org.bukkit.entity.Entity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
 
 public class EntityDamage implements Listener {
     @EventHandler
-    public void onEntityDamage(org.bukkit.event.entity.EntityDamageEvent event){
+    public void onEntityDamage(EntityDamageEvent event){
         if(!event.isCancelled() && ConfigHandler.getData().getBoolean("god." + event.getEntity().getUniqueId())){
             event.setCancelled(true);
         }
--- src/main/java/com/github/zac694/usefulcommands/events/PlayerJoin.java
+++ src/main/java/com/github/zac694/usefulcommands/events/PlayerJoin.java
@@ -5,11 +5,13 @@ import com.github.zac694.usefulcommands.UsefulCommands;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 
 public class PlayerJoin implements Listener {
     @EventHandler
-    public void onPlayerJoin(org.bukkit.event.player.PlayerJoinEvent event){
-        Bukkit.getOnlinePlayers().forEach(player -> {if(ConfigHandler.getData().getStringList("vanish").contains(player.getUniqueId().toString())){
+    public void onPlayerJoin(PlayerJoinEvent event){
+        Bukkit.getOnlinePlayers().forEach(player -> {
+            if (ConfigHandler.getData().getStringList("vanish").contains(player.getUniqueId().toString())) {
             event.getPlayer().hidePlayer(UsefulCommands.getMainClass(), player);
         }});
     }

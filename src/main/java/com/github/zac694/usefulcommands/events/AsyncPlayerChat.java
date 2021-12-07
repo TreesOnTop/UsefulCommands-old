66abaceadd3038271c2942fb4b36c208e4e4cea2
Zac694 <zmcnabb@gmail.com>
2021-12-07 15:00:09 -0500

    made some events more consistent

--- src/main/java/com/github/zac694/usefulcommands/events/AsyncPlayerChat.java
+++ src/main/java/com/github/zac694/usefulcommands/events/AsyncPlayerChat.java
@@ -3,10 +3,11 @@ package com.github.zac694.usefulcommands.events;
 import com.github.zac694.usefulcommands.ConfigHandler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 
 public class AsyncPlayerChat implements Listener {
     @EventHandler
-    public void onAsyncPlayerChat(org.bukkit.event.player.AsyncPlayerChatEvent event){
+    public void onAsyncPlayerChat(AsyncPlayerChatEvent event){
         if(!event.isCancelled() && ConfigHandler.getData().getBoolean("muted." + event.getPlayer().getUniqueId()) || !event.isCancelled() && ConfigHandler.getData().getLong("tmuted." + event.getPlayer().getUniqueId()) >= System.currentTimeMillis()){
             event.setCancelled(true);
             event.getPlayer().sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "§cYou are muted");
--- src/main/java/com/github/zac694/usefulcommands/events/PlayerMove.java
+++ src/main/java/com/github/zac694/usefulcommands/events/PlayerMove.java
@@ -3,10 +3,11 @@ package com.github.zac694.usefulcommands.events;
 import com.github.zac694.usefulcommands.ConfigHandler;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMove implements Listener {
     @EventHandler
-    public void onPlayerMove(org.bukkit.event.player.PlayerMoveEvent event){
+    public void onPlayerMove(PlayerMoveEvent event){
         if(!event.isCancelled() && ConfigHandler.getData().getBoolean("frozen." + event.getPlayer().getUniqueId())){
             event.getPlayer().sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "§cYou are frozen");
             event.setCancelled(true);

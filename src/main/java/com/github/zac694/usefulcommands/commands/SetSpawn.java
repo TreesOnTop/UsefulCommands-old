7df3ac733261f145ff265b533b250f7d085d3e32
Zac694 <zmcnabb@gmail.com>
2021-12-22 15:06:29 -0500

    fixed spawn in 1.13

--- src/main/java/com/github/zac694/usefulcommands/commands/SetSpawn.java
+++ src/main/java/com/github/zac694/usefulcommands/commands/SetSpawn.java
@@ -5,31 +5,29 @@ import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.DoubleArgument;
 import dev.jorel.commandapi.arguments.FloatArgument;
 import dev.jorel.commandapi.arguments.StringArgument;
-import org.bukkit.Bukkit;
-import org.bukkit.Location;
-import org.bukkit.World;
 
 public class SetSpawn {
     public static void register(){
         new CommandAPICommand("setspawn")
                 .withPermission("usefulcommands.setspawn")
                 .executesPlayer((sender, args) -> {
-                    double x = sender.getLocation().getBlockX()+0.5;
-                    double y = sender.getLocation().getBlockY();
-                    double z = sender.getLocation().getBlockZ()+0.5;
-                    World world = sender.getWorld();
+                    ConfigHandler.getData().set("spawn.world", sender.getWorld().getName());
+                    ConfigHandler.getData().set("spawn.x", sender.getLocation().getBlockX()+0.5);
+                    ConfigHandler.getData().set("spawn.y", sender.getLocation().getBlockY());
+                    ConfigHandler.getData().set("spawn.z", sender.getLocation().getBlockZ()+0.5);
                     int yaw = Math.round(sender.getLocation().getYaw());
                     if(yaw <= 45 && yaw >= -45){
-                        ConfigHandler.getData().set("spawn", new Location(world,x,y,z,0,0));
+                        ConfigHandler.getData().set("spawn.yaw", 0);
                     }else if(yaw >= -135 && yaw <= -45){
-                        ConfigHandler.getData().set("spawn", new Location(world,x,y,z,-90,0));
+                        ConfigHandler.getData().set("spawn.yaw", -90);
                     }else if(yaw <= 135 && yaw >= 45){
-                        ConfigHandler.getData().set("spawn", new Location(world,x,y,z,90,0));
+                        ConfigHandler.getData().set("spawn.yaw", 90);
                     }else{
-                        ConfigHandler.getData().set("spawn", new Location(world,x,y,z,180,0));
+                        ConfigHandler.getData().set("spawn.yaw", 180);
                     }
+                    ConfigHandler.getData().set("spawn.pitch", 0);
                     ConfigHandler.save();
-                    sender.sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "Set spawn to " + x + ", " + y + ", " + z);
+                    sender.sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "Set spawn to " + sender.getLocation().getBlockX() + ", " + sender.getLocation().getBlockY() + ", " + sender.getLocation().getBlockZ());
                 }).register();
         new CommandAPICommand("setspawn")
                 .withPermission("usefulcommands.setspawn")
@@ -38,9 +36,12 @@ public class SetSpawn {
                 .withArguments(new DoubleArgument("z"))
                 .withArguments(new StringArgument("world"))
                 .executes((sender, args) -> {
-                    double x = (double)args[0]+0.5;
-                    double z = (double)args[2]+0.5;
-                    ConfigHandler.getData().set("spawn", new Location(Bukkit.getServer().getWorld(args[3].toString()),x,(double)args[1],z,0,0));
+                    ConfigHandler.getData().set("spawn.world", args[3]);
+                    ConfigHandler.getData().set("spawn.x", (double)args[0]+0.5);
+                    ConfigHandler.getData().set("spawn.y", args[1]);
+                    ConfigHandler.getData().set("spawn.z", (double)args[2]+0.5);
+                    ConfigHandler.getData().set("spawn.yaw", 0);
+                    ConfigHandler.getData().set("spawn.pitch", 0);
                     ConfigHandler.save();
                     sender.sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "Set spawn to " + args[0] + ", " + args[1] + ", " + args[2] + " in world " + args[3]);
                 }).register();
@@ -53,9 +54,12 @@ public class SetSpawn {
                 .withArguments(new FloatArgument("pitch"))
                 .withArguments(new StringArgument("world"))
                 .executes((sender, args) -> {
-                    double x = (double)args[0]+0.5;
-                    double z = (double)args[2]+0.5;
-                    ConfigHandler.getData().set("spawn", new Location(Bukkit.getServer().getWorld(args[5].toString()),x,(double)args[1],z,(float)args[3],(float)args[4]));
+                    ConfigHandler.getData().set("spawn.world", args[5]);
+                    ConfigHandler.getData().set("spawn.x", (double)args[0]+0.5);
+                    ConfigHandler.getData().set("spawn.y", args[1]);
+                    ConfigHandler.getData().set("spawn.z", (double)args[2]+0.5);
+                    ConfigHandler.getData().set("spawn.yaw", args[3]);
+                    ConfigHandler.getData().set("spawn.pitch", args[4]);
                     ConfigHandler.save();
                     sender.sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "Set spawn to " + args[0] + ", " + args[1] + ", " + args[2] + " with yaw " + args[3] + " and pitch " + args[4] + " in world " + args[5]);
                 }).register();
--- src/main/java/com/github/zac694/usefulcommands/commands/Spawn.java
+++ src/main/java/com/github/zac694/usefulcommands/commands/Spawn.java
@@ -3,6 +3,9 @@ package com.github.zac694.usefulcommands.commands;
 import com.github.zac694.usefulcommands.ConfigHandler;
 import dev.jorel.commandapi.CommandAPICommand;
 import dev.jorel.commandapi.arguments.PlayerArgument;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.World;
 import org.bukkit.entity.Player;
 
 import java.util.Objects;
@@ -13,7 +16,8 @@ public class Spawn{
                 .withPermission("usefulcommands.spawn")
                 .executesPlayer((sender, args) -> {
                     if(ConfigHandler.getData().contains("spawn")){
-                        sender.teleport(Objects.requireNonNull(ConfigHandler.getData().getLocation("spawn")));
+                        sender.sendMessage("test");
+                        teleport(sender);
                         sender.sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "Sent you to spawn");
                     }
                     else{
@@ -22,15 +26,27 @@ public class Spawn{
                 }).register();
         new CommandAPICommand("spawn")
                 .withArguments(new PlayerArgument("player").withPermission("usefulcommands.spawn.others"))
-                .executesPlayer((sender, args) -> {
+                .executes((sender, args) -> {
                     if(ConfigHandler.getData().contains("spawn")){
                         Player p = (Player)args[0];
-                        p.teleport(Objects.requireNonNull(ConfigHandler.getData().getLocation("spawn")));
+                        teleport(p);
                         p.sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "You were sent to spawn");
+                        sender.sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "Sent" + p.getName() + "to spawn");
                     }
                     else{
                         sender.sendMessage(ConfigHandler.getConfig().getString("OutputPrefix") + "Spawn isn't set");
                     }
                 }).register();
+    }
+
+    private static void teleport(Player p) {
+        World w = Bukkit.getServer().getWorld(Objects.requireNonNull(ConfigHandler.getData().getString("spawn.world")));
+        double x = ConfigHandler.getData().getDouble("spawn.x");
+        double y = ConfigHandler.getData().getDouble("spawn.y");
+        double z = ConfigHandler.getData().getDouble("spawn.z");
+        float yaw = (float)ConfigHandler.getData().getDouble("spawn.yaw");
+        float pitch = (float)ConfigHandler.getData().getDouble("spawn.pitch");
+        Location loc = new Location(w, x, y, z, yaw, pitch);
+        p.teleport(loc);
     }
 }

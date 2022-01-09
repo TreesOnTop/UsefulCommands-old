03b46ae2ac26ce356311b9aef68e0ee34a585b70
Zac694 <zmcnabb@gmail.com>
2022-01-08 19:17:12 -0500

    added /setspawn, /spawn, and /tempban

--- src/main/java/com/github/zac694/usefulcommands/events/PlayerJoin.java
+++ src/main/java/com/github/zac694/usefulcommands/events/PlayerJoin.java
@@ -7,9 +7,14 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
+import java.util.Objects;
+
 public class PlayerJoin implements Listener {
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event){
+        if(ConfigHandler.getData().contains("spawn") && ConfigHandler.getConfig().getBoolean("SpawnOnJoin")){
+            event.getPlayer().teleport(Objects.requireNonNull(ConfigHandler.getData().getLocation("spawn")));
+        }
         if(ConfigHandler.getData().getStringList("vanish").contains(event.getPlayer().getUniqueId().toString())){
             Bukkit.getOnlinePlayers().forEach(player -> event.getPlayer().hidePlayer(UsefulCommands.getMainClass(), player));
         }
